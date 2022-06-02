8884153ae02ce3d34461cf24fb11f80be2ca6562
75211 <trierickceraptor@hotmail.com>
2022-06-01 20:42:22 -0500

    Añadido Ciclo de Vida

--- app.js
+++ app.js
@@ -20,6 +20,8 @@ var imagenesRouter = require('./routes/imagenes');
 var contenedoresRouter = require('./routes/contenedores');
 var dockerfilesRouter = require('./routes/dockerfiles');
 var directivasdockerfileRouter = require('./routes/directivasdockerfile');
+var ciclovidaRouter = require('./routes/ciclovida');
+
 var autosRouter = require('./routes/autos');//controlador de rutas del EndPoint autos
 
 
@@ -43,6 +45,7 @@ app.use('/imagenes', imagenesRouter);
 app.use('/contenedores', contenedoresRouter);
 app.use('/dockerfiles', dockerfilesRouter);
 app.use('/directivasdockerfile', directivasdockerfileRouter);
+app.use('/ciclovida', ciclovidaRouter);
 app.use('/autos',autosRouter);// /autosRouter va hacer  la APIREST y el controlador es autosRouter
 
 
